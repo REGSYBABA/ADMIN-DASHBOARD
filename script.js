b5f3960ff2b93e9a06556de02a4bc73d768ded25
REGSYBABA <sumimichael77@gmail.com>
2023-09-07 22:22:00 +0100

    Just added little functionalities for my dashboard

---
+++ script.js
@@ -0,0 +1,28 @@
+const body = document.querySelector("body")
+const sidebar = body.querySelector(".sidebar")
+const arrow = body.querySelector(".arrow")
+const searchBtn = body.querySelector(".search-box")
+const modeSwitch = body.querySelector(".toggle-switch")
+const modeText = body.querySelector(".mode-text") 
+const sun = document.querySelector('.sun')
+const moon = document.querySelector('.moon')
+
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle('dark')
+    console.log('balls')
+
+    if(body.classList.contains('dark')){
+        modeText.textContent = "Light Mode"
+        sun.style.opacity = 1;
+        moon.style.opacity = 0;
+    } else{
+        modeText.textContent = "Dark Mode"
+        moon.style.opacity = 1;
+        sun.style.opacity = 0;
+    }
+})
+
+arrow.addEventListener("click", () => {
+    sidebar.classList.toggle('close')
+    console.log('balls')
+})
